0327cd80852e316d4d48b0a01ef303e29b74183a
Jesse Christensen <jtq.christensen@gmail.com>
2022-10-13 12:13:42 -0700

    React version fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import Auth0ProviderWithHistory from './utils/auth/auth0-provider';
 
 import './index.css';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Auth0ProviderWithHistory>
-      <App />
-    </Auth0ProviderWithHistory>
-  </BrowserRouter>,
-  document.getElementById("root")
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <Auth0ProviderWithHistory>
+        <App />
+      </Auth0ProviderWithHistory>
+    </BrowserRouter>
+  </StrictMode>
 );
 
 
