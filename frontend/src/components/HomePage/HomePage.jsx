448fb5a20082b8bb12a887cebe463fd978539d44
Braxton Kappes <braxtonkappes@gmail.com>
2022-10-18 16:56:14 -0500

    added a bunch of layout for homepage

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,14 +1,34 @@
 import './HomePage.css'
+// eslint-disable-next-line
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
 
 function HomePage() {
-  const dispatch = useDispatch();
 
   return (
     <div className='homepage-wrapper'>
+      <div className="homepage-banner-body">
+        <img src="" alt="" className="homepage-banner-img" />
+        <div className="store-navbar-body">
+          <div className="store-navbar-content">
+            <div className="store-navbar-links">
+              <h3>Your Store</h3>
+              <h3>New & Noteworthy</h3>
+              <h3>Categories</h3>
+              <h3>Points Shop</h3>
+              <h3>News</h3>
+              <h3>Labs</h3>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="homepage-body">
-        <div className="homepage-content">
+        <div className="homepage-gutter">
+          <div className="homepage-gutter-block">
+            <div className="homepage-gutter-header">
+              <a href=""></a>
+              <br />Gift Cards
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,27 +1,25 @@
 import './NavBar.css'
 import React from 'react'
-// import { useDispatch } from 'react-redux';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function NavBar() {
-  // const dispatch = useDispatch();
   const { loginWithRedirect } = useAuth0();
 
   return (
     <div className='navbar-wrapper'>
       <div className="navbar-body">
         <div className="navbar-content">
-        <button
-        className="signup-btn"
-        onClick={() =>
-        loginWithRedirect({
-          screen_hint: 'signup',
-          redirectUri: 'http://localhost:3000/complete_registration'
-        })
-      }
-    >
-      Sign Up
-    </button>
+          <button
+          className="signup-btn"
+          onClick={() =>
+          loginWithRedirect({
+            screen_hint: 'signup',
+            redirectUri: 'http://localhost:3000/complete_registration'
+            })
+          }
+          >
+          Sign Up
+          </button>
         </div>
       </div>
     </div>
