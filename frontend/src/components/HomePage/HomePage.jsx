250a876c489dc3a3ed3bbf07730d5a90bbf819d5
Braxton Kappes <braxtonkappes@gmail.com>
2022-10-20 15:20:22 -0500

    homepage in the works

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -24,17 +24,49 @@ function HomePage() {
       <div className="homepage-body">
         <div className="homepage-gutter">
           <div className="homepage-gutter-block">
-            <div className="homepage-gutter-header-1">
+            <div className="homepage-gutter-header">
               <a href="?"></a>
               <br />
               <h1 className="homepage-gutter-header-title">GIFT CARDS</h1>
-              <a href="?">Now Available on Steam</a>
+              <a href="?">Now Available on Frosty</a>
             </div>
-            <div className="homepage-gutter-header-2">
+            <div className="homepage-gutter-header">
               <h1 className="homepage-gutter-header-title">RECOMMENDED</h1>
               <a href="?">By Friends</a>
               <a href="?">By Curators</a>
               <a href="?">Tags</a>
+            </div>
+            <div className="homepage-gutter-header">
+              <h1 className="homepage-gutter-header-title">DISCOVERY QUEUES</h1>
+              <a href="?">Recommendations</a>
+              <a href="?">New Releases</a>
+            </div>
+            <div className="homepage-gutter-header">
+              <h1 className="homepage-gutter-header-title">BROWSE CATEGORIES</h1>
+              <a href="?">Top Sellers</a>
+              <a href="?">Upcoming</a>
+              <a href="?">Specials</a>
+              <a href="?">VR Titles</a>
+              <a href="?">Controller-Friendly</a>
+            </div>
+            <div className="homepage-gutter-header">
+              <h1 className="homepage-gutter-header-title">BROWSE BY GENRE</h1>
+              <a href="?">Free to Play</a>
+              <a href="?">Early Access</a>
+              <a href="?">Action</a>
+              <a href="?">Adventure</a>
+              <a href="?">Casual</a>
+              <a href="?">Indie</a>
+              <a href="?">Massively Multiplayer</a>
+              <a href="?">Racing</a>
+              <a href="?">RPG</a>
+              <a href="?">Simulation</a>
+              <a href="?">Sports</a>
+              <a href="?">Strategy</a>
+            </div>
+            <div className="homepage-gutter-header">
+              <h1 className="homepage-gutter-header-title">RECENTLY VIEWED</h1>
+              /* query database for recently viewed */
             </div>
           </div>
         </div>
