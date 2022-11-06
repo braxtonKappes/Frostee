4ecfb376c2819c2477a3ba18dc99e1408659f6c2
Braxton Kappes <braxtonkappes@gmail.com>
2022-11-06 01:02:53 -0600

    small changes

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -4,18 +4,21 @@ import React, { useState } from 'react'
 import HomePageGutter from '../HomePageGutter'
 import SearchNavBar from '../SearchNavBar'
 import './HomePage.css'
+import Carousel from '../Carousel/Carousel.js'
 
 function HomePage() {
   return (
     <div className='homepage-wrapper'>
       <SearchNavBar/>
-      <div className="homepage-banner-body">
-        <img src="" alt="" className="homepage-banner-img" />
-      </div>
-      <HomePageGutter />
-      <div className="homepage-body">
-        <div className="homepage-featured-recommended">
-          
+      <div className="homepage-contents">
+        <div className="homepage-banner-body">
+          <img src="" alt="" className="homepage-banner-img" />
+        </div>
+        <HomePageGutter />
+        <div className="homepage-body">
+          <div className="homepage-featured-recommended">
+            <Carousel />
+          </div>
         </div>
       </div>
     </div>
